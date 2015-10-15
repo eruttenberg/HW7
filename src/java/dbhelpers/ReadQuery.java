51682993b6e2d40e81580f9276c78ee54e476d89
Eric Ruttenberg <eric-ruttenberg@uiowa.edu>
2015-10-14 19:33:29 -0500

    add read,jsp and read.java, as well as readQuery.java. the files all intereact with each other and the database now and the website should be functioning.

---
+++ src/java/dbhelpers/ReadQuery.java
@@ -0,0 +1,114 @@
+
+package dbhelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Movies;
+
+public class ReadQuery {
+    
+    private Connection conn;
+    private ResultSet results;
+    
+    public ReadQuery() {
+    
+    Properties props = new Properties();
+    InputStream instr = getClass().getResourceAsStream("dbConnection.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    String driver = props.getProperty("driver.name");
+    String url = props.getProperty("server.name");
+    String username = props.getProperty("user.name");
+    String password = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url,username,password);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    public void doRead() {
+        try {
+            String query = "Select * from movies";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    
+    public String getHTMLtable() {
+        String table = "";
+        table += "<table border = 3>";
+        
+        try {
+            while(this.results.next()) {
+                
+                Movies movies = new Movies();
+                movies.setMovieID(this.results.getInt("movieID"));
+                movies.setMovieTitle(this.results.getString("movieTitle"));
+                movies.setMovieYear(this.results.getInt("movieYear"));
+                movies.setMovieGenre(this.results.getString("movieGenre"));
+                movies.setDirector(this.results.getString("director"));
+                
+                table += "<tr>";
+                table += "<td>";
+                table += movies.getMovieID();                  
+                table += "</td>";
+                
+                table += "<td>";
+                table += movies.getMovieTitle();          
+                table += "</td>";
+               
+                table += "<td>";
+                table += movies.getMovieYear();            
+                table += "</td>";
+               
+                table += "<td>";
+                table += movies.getMovieGenre();          
+                table += "</td>";
+                
+                table += "<td>";
+                table += movies.getDirector();        
+                table += "</td>";
+                table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table += "</table>";
+        
+                return table;
+                
+        
+        
+    }
+    
+}
